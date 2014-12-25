da379b58b5dbe115eeca6ba05eb28e85d4c22579
Alain Dutech <alain.dutech@loria.fr>
2014-12-25 12:39:19 +0100

    INIT of GDXInertia

---
+++ desktop/src/com/game/snowgoon/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.game.snowgoon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.game.snowgoon.GDXInertia;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GDXInertia(), config);
+	}
+}
